8806e152071c52c8ef227b31f113cad2b2e524f9
Paul Karels <pkarels@nerdery.com>
2020-12-17 18:11:16 -0600

    Add tab layout

--- app/src/main/java/com/playground/navigationwithtabs/IntroFragment.kt
+++ app/src/main/java/com/playground/navigationwithtabs/IntroFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 
 /**
  * A simple [Fragment] subclass.
@@ -16,7 +17,16 @@ class IntroFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intro, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_intro, container, false)
+        val nextButton: View = rootView.findViewById(R.id.intro_button_next)
+
+        nextButton.setOnClickListener { v ->
+            val action = IntroFragmentDirections.actionIntroFragmentToViewPagerFragment()
+            v.findNavController().navigate(action)
+        }
+
+        return rootView
     }
 }
